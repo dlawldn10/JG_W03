30a7163430e62c4deacdfc1d0a1b443909ecada6
Jiwoo Lim <dlawldn10@gmail.com>
2022-10-09 23:16:20 +0900

    Solve 12~16

--- JW/W03_jwoo.py
+++ JW/W03_jwoo.py
@@ -512,7 +512,7 @@
 
 
 
-# #dfs, bfs 혼용한 코드
+# bfs 코드
 # import sys
 # from collections import deque
 # read = sys.stdin.readline
@@ -585,3 +585,314 @@
 
 # # 출력
 # print(answer)
+
+
+# #2671
+# #정답 코드
+# import sys
+# input = sys.stdin.readline
+# sys.setrecursionlimit(10**4)
+
+# N, M = map(int, input().split())
+
+# adj = [[] for _ in range(N+1)]
+# adj2 = [[] for _ in range(N+1)]
+
+# for _ in range(M):
+#     a, b = map(int, input().split())
+#     adj[a].append(b)
+#     adj2[b].append(a)
+
+# # print(adj)
+
+# mid = (N+1)/2
+
+# def dfs(arr, v):
+#     global cnt
+#     for i in arr[v]:
+#         if chk[i] == 0:
+#             chk[i] = 1
+#             cnt += 1
+#             dfs(arr, i)
+
+
+# ans = 0
+# for i in range(1, N+1):
+#     chk = [0]*(N+1)
+#     cnt = 0
+#     dfs(adj, i)
+#     if cnt >= mid:
+#         ans += 1
+        
+#     cnt = 0
+#     dfs(adj2, i)
+#     if cnt >= mid:
+#         ans += 1
+        
+# print(ans)
+
+
+# #2178
+# from collections import deque
+# import sys
+
+# dy = (1, 0, -1, 0)
+# dx = (0, -1, 0, 1)
+
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+
+# graph = [input().rstrip() for _ in range(N)]
+
+# chk = [[False]*M for _ in range(N)]
+# # print(chk)
+# # print(graph)
+
+# def is_valid_coord(x, y):
+#     return 0 <= y < M and 0 <= x < N
+
+# def bfs(sx, sy, sd):
+#     global ans
+
+#     q = deque()
+#     chk[sx][sy] = True
+#     q.append((sx, sy, sd))
+
+#     while len(q):
+#         x, y, d = q.popleft()
+
+#         if x == N-1 and y == M-1:
+#             print(d)
+#             break
+
+#         for j in range(4):
+#             nx = x + dx[j]
+#             ny = y + dy[j]
+#             nd = d + 1
+#             # print(nx, ny)
+#             if is_valid_coord(nx, ny) and not chk[nx][ny] and graph[nx][ny] == '1':
+#                 chk[nx][ny] = True
+#                 q.append((nx, ny, nd))
+
+# bfs(0, 0, 1)
+
+
+# #18352
+# #풀이2
+# #7:26
+# #8:48
+# from collections import deque
+# import sys
+
+# dy = (1, 0, -1, 0)
+# dx = (0, -1, 0, 1)
+
+# input = sys.stdin.readline
+# N, M, K, X = map(int, input().split())
+
+# graph = [[] for _ in range(N+1)]
+# for i in range(1, 1+M):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+
+# chk = [-1]*(N+1)
+# #방문 안했을 때 -1
+# #방문 했을 때 0
+# #다른 곳을 탐색할때 방문했던 곳이 또다른 최단거리 도시였을 수 있음.
+# #-> 이미 방문한 지점 다시 방문 시 d값을 비교하여 최단거리로 갱신해줌
+
+
+# q = deque()
+# chk[X] = 0
+# q.append((X, 1))
+
+# ans = []
+# while len(q):
+#     x, d = q.popleft()
+
+#     if d > K:
+#         continue
+        
+#     for j in graph[x]:
+#         if chk[j] == -1:
+#             chk[j] = d
+#             q.append((j, d+1))
+#         elif chk[j] > d:
+#             chk[j] = d
+#             q.append((j, d+1))
+
+# #모두 탐색했는데 도시 아무것도 없을 때
+# if K not in chk:
+#     print(-1)
+
+# for i in range(1, N+1):
+#     if chk[i] == K:
+#         print(i)
+
+
+# #풀이 1
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+# N, M, K, X = map(int, input().split())
+
+# graph = [[] for _ in range(N+1)]
+# for i in range(1, 1+M):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+
+# chk = [False]*(N+1)
+# # print(graph)
+# # print(chk)
+
+
+# q = deque()
+# chk[X] = True
+# q.append((X, 0))
+
+# ans = []
+
+# while len(q):
+#     x, d = q.popleft()
+
+#     if d == K:
+#         # print(x)
+#         ans.append(x)
+#         # print(chk)
+#         d = 1
+#         continue
+        
+#     for j in graph[x]:
+#         if not chk[j]:
+#             chk[j] = True
+#             q.append((j, d+1))
+
+# #모두 탐색했는데 도시 아무것도 없을 때
+# if len(ans) == 0:
+#     print(-1)
+# else:
+#     for i in sorted(ans):
+#         print(i)
+# # print(chk)
+
+
+#1916
+# #시간초과
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+# N = int(input())
+# M = int(input())
+
+# graph = [[] for _ in range(N+1)]
+# for i in range(1, 1+M):
+#     a, b, c = map(int, input().split())
+#     graph[a].append([b,c])
+
+# S, E = map(int, input().split())
+# chk = [False]*(N+1)
+
+# q = deque()
+# chk[S] = True
+# q.append([S, 0])
+
+# ans = []
+
+# while len(q):
+#     #현재 도시, 비용
+#     x, cost = q.popleft()
+
+#     if x == E:
+#         # print(x)
+#         print(cost)
+#         break
+        
+#     tmp = 100000
+#     idx = 0
+#     #비용이 최소인 방향으로 이동해야함.
+#     for j in graph[x]:
+#         if not chk[j[0]] and j[1] < tmp:
+#             tmp = j[1]
+#             idx = j[0]
+#     chk[idx] = True
+#     q.append([idx, cost+tmp])
+    
+
+# #정답 코드
+# #다익스트라 알고리즘
+# import heapq
+# from sys import maxsize
+# import sys
+
+
+# input = sys.stdin.readline
+
+# n = int(input())
+# m = int(input())
+
+# graph = [[] for _ in range(n + 1)]
+# visited = [maxsize] * (n + 1)
+# for _ in range(m):
+#     a, b, c = map(int, input().split())
+#     graph[a].append((c, b))
+
+# start, end = map(int, input().split())
+
+
+# def dijkstra(x):
+#     pq = []
+#     heapq.heappush(pq, (0, x))
+#     visited[x] = 0
+
+#     while pq:
+#         d, x = heapq.heappop(pq)
+
+#         if visited[x] < d:
+#             continue
+
+#         for nw, nx in graph[x]:
+#             nd = d + nw
+
+#             if visited[nx] > nd:
+#                 heapq.heappush(pq, (nd, nx))
+#                 visited[nx] = nd
+
+
+# dijkstra(start)
+
+# print(visited[end])
+
+
+#2665
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+n = int(input())
+room = []
+for _ in range(n):
+    room.append(list(map(int, input().rstrip())))
+visit = [[0] * n for _ in range(n)]
+
+def dijkstra():
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+    heap = []
+    heappush(heap, [0, 0, 0])
+    visit[0][0] = 1
+    while heap:
+        a, x, y = heappop(heap)
+        if x == n - 1 and y == n - 1:
+            print(a)
+            return
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0:
+                visit[nx][ny] = 1
+                if room[nx][ny] == 0:
+                    heappush(heap, [a + 1, nx, ny])
+                else:
+                    heappush(heap, [a, nx, ny])
+
+dijkstra()
